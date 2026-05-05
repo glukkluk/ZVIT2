57e4b361384a6d246495f283a221a81f120ddecb
GlukKluk <glukkluk.dev@gmail.com>
2026-05-05 14:12:55 +0300

    feat(config): :wrench: Add configuration settings and environment variables
    Pydantic-setting is used to configure the settings. A template for the environment variables file has also been created.

---
+++ Python/src/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    database_url: str
+    secret_key: str
+    algorithm: str
+
+
+settings = Settings()
