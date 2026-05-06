b4354b6a8d1d7d2e05c0f82401157c91803cb0e7
GlukKluk <glukkluk.dev@gmail.com>
2026-05-06 14:16:01 +0300

    feat(route): :sparkles: Enhance routing with registration view and notifications

--- Python/src/main.py
+++ Python/src/main.py
@@ -1,17 +1,45 @@
 import flet as ft
 
-from views import HomeView, LoginView
+from components import AlreadyAuthNotification, NotAuthNotification
+from views import HomeView, LoginView, RegisterView
 
 
-def main(page: ft.Page):
+async def main(page: ft.Page):
     page.title = "Розумний розклад"
 
-    def route_change():
-        page.views.clear()
-        page.views.append(HomeView("/"))
+    page.session.store.set(key="first_entry", value=True)
 
-        if page.route == "/login":
-            page.views.append(LoginView("/setting"))
+    async def route_change():
+        page.views.clear()
+
+        is_authorized = page.session.store.get(key="authorized")
+        first_entry = page.session.store.get(key="first_entry")
+
+        match page.route:
+            case "/":
+                if is_authorized:
+                    page.views.append(HomeView("/"))
+                else:
+                    await page.push_route("/login")
+
+                    if not first_entry:
+                        page.show_dialog(dialog=NotAuthNotification())
+
+                    page.session.store.set(key="first_entry", value=False)
+
+            case "/login":
+                if is_authorized:
+                    await page.push_route("/")
+                    page.show_dialog(dialog=AlreadyAuthNotification())
+                else:
+                    page.views.append(LoginView("/login"))
+
+            case "/register":
+                if is_authorized:
+                    await page.push_route("/")
+                    page.show_dialog(dialog=AlreadyAuthNotification())
+                else:
+                    page.views.append(RegisterView("/register"))
 
         page.update()
 
@@ -25,7 +53,7 @@ def main(page: ft.Page):
     page.on_route_change = route_change
     page.on_view_pop = view_pop
 
-    route_change()
+    await route_change()
 
 
 ft.run(main, view=ft.AppView.WEB_BROWSER)
