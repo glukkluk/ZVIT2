4aadf73f7bcbbe8ae37b8dc0038c2be003b14427
GlukKluk <glukkluk.dev@gmail.com>
2026-05-06 14:10:19 +0300

    feat(login): :recycle: Refactor LoginView
    Now LoginView use class attributes for form fields.
    Also implement routing to login and reset password (in future) pages

--- Python/src/views/login.py
+++ Python/src/views/login.py
@@ -3,19 +3,39 @@ import flet as ft
 
 class LoginView(ft.View):
     def __init__(self, path: str):
+        self.email_field = ft.TextField(value="E-mail")
+        self.password_field = ft.TextField(value="Пароль")
+        self.remember_user_checkbox = ft.Checkbox(label="Запам'ятати мене")
+
         super().__init__(
             route=path,
             controls=[
                 ft.Text(value="Вхід"),
-                ft.TextField(value="E-mail"),
-                ft.TextField(value="Пароль"),
-                ft.Checkbox(label="Запам'ятати мене"),
+                self.email_field,
+                self.password_field,
+                self.remember_user_checkbox,
                 ft.Button(content="Увійти"),
-                ft.Text(spans=[ft.TextSpan(text="Створити акаунт", url="/register")]),
                 ft.Text(
                     spans=[
-                        ft.TextSpan(text="Не пам'ятаю пароль", url="/reset-password")
+                        ft.TextSpan(
+                            text="Створити акаунт",
+                            on_click=self.go_to_register,
+                        )
+                    ]
+                ),
+                ft.Text(
+                    spans=[
+                        ft.TextSpan(
+                            text="Не пам'ятаю пароль",
+                            on_click=self.go_to_reset_password,
+                        )
                     ]
                 ),
             ],
         )
+
+    async def go_to_register(self):
+        await self.page.push_route("/register")
+
+    async def go_to_reset_password(self):
+        await self.page.push_route("/reset-password")
