ccbd1949fe42d1509b8db711bc807473a3c93b6c
GlukKluk <glukkluk.dev@gmail.com>
2026-05-12 08:19:20 +0300

    refactor: ♻️ Update BaseView to use instance variables for APP_BAR and RAIL

--- Python/src/components/base_view.py
+++ Python/src/components/base_view.py
@@ -4,22 +4,21 @@ from .appbar import MainAppBar
 from .rail import MainRail
 
 
-APP_BAR = MainAppBar()
-RAIL = MainRail(index=0)
-
-
 class BaseView(ft.View):
+    APP_BAR = MainAppBar()
+    RAIL = MainRail(index=0)
+
     def __init__(self, route: str, body: list[ft.BaseControl], **kwargs):
         super().__init__(
             route=route,
-            appbar=APP_BAR,
+            appbar=self.APP_BAR,
             controls=[
                 ft.SafeArea(
                     expand=True,
                     content=ft.Row(
                         expand=True,
                         controls=[
-                            ft.SelectionArea(content=RAIL),
+                            ft.SelectionArea(content=self.RAIL),
                             ft.VerticalDivider(width=1),
                             ft.Column(
                                 alignment=ft.MainAxisAlignment.START,
